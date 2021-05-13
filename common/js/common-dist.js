ac566f492c68c006b290baebf316a5d6c848e83a
jun tamagawa <jun@rfs.jp>
2021-05-13 19:59:03 +0900

    微修正

--- common/js/common-dist.js
+++
@@ -1,2 +0,0 @@
-var $=function(n){return document.querySelector(n)};document.addEventListener("DOMContentLoaded",function(n){}),window.addEventListener("load",function(n){});
-//# sourceMappingURL=common-dist.js.map
---
+++ common/js/index.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(n){})),window.addEventListener("load",(function(n){}));
+//# sourceMappingURL=index.js.map
